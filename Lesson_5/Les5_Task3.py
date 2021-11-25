e6b7c2ea1c8d8b1d4d7220b54b3638ae390d62d7
AvokrichA <leysanspb@mail.ru>
2021-11-25 17:08:46 +0300

    l5

--- Lesson_5/Les5_Task3.py
+++ Lesson_5/Les5_Task3.py
@@ -1,17 +1,18 @@
 with open('L5_t3.txt', 'r', encoding='UTF-8') as my_file:
-    my_list=tuple(my_file.read().split('\n'))
-    print(my_list, sep=':')
+    i = 0
+    s = 0
+    print('Список сотрудников с окладом менее 20000:')
+    while True:
+        my_list = my_file.readline()
+        if not my_list:
+            break
+        if float(my_list.split(' ')[1]) < 20000:
+            print(my_list.split(' ')[0])
+        s += float(my_list.split(' ')[1])
+        i += 1
+
+    print('Средняя величина дохода сотрудников:', s/i)
 
 
 
 
-    # sal = []
-    # poor = []
-    # my_list = my_file.read().split('\n')
-    # print(my_list)
-    # for i in my_list:
-    #     i = i.split()
-    #     if float(i[::2]) < 20000:
-    #         poor.append(i[0])
-    #         sal.append(i[::2])
-    # print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(float, sal)) / len(sal)}')
