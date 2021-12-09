3be293dfc723fb99f103d70da628966e5ed9d1fa
AvokrichA <leysanspb@mail.ru>
2021-12-09 23:18:26 +0300

    Les7 t2

---
+++ Lesson_7/L7_T2.py
@@ -0,0 +1,50 @@
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
+# К типам одежды в этом проекте относятся пальто и костюм.
+# У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
+# Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы:
+# для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
+# реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора
+# @property.
+
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    @abstractmethod
+    def consumption(self):
+        pass
+
+class Coat(Clothes):
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def consumption(self):
+        return self.size / 6.5 + 0.5
+
+class Costume(Clothes):
+    def __init__(self, size):
+       self.size = size
+
+    @property
+    def consumption(self):
+        return  2 * self.size  + 0.3
+
+    def total_consumption(self, total_list):
+        i=0
+        for j in total_list:
+            i+=total_consumption
+            return i
+
+
+
+
+coat = Coat(1)
+costume = Costume(1.98)
+print(coat.consumption)
+print(costume.consumption())
+print(coat.abstract())
